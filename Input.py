dac2f09a49185ebbc46e624db947379c8ad44f46
Ivan Contreras <a01540379@itesm.mx>
2020-05-21 15:29:40 -0500

    Ahora Input toma los argumentos para ejecutar las acciones adecuadas

--- Input.py
+++ Input.py
@@ -1,57 +1,57 @@
 import re
 import csv
 
-
-
-#example1 =  '''
-#            "Clouds are white."
-#            "Pittsburgh is beautiful."
-#            '''
-
-#example2 = '''
-#            """Clouds,are,white."""
-#            """Pittsburg,is,beautiful."""
-#            '''
-
-
 class Input():
     
-    def process(self, fileString, stopFile):
-        file = open(stopFile, "r")
-        stopWords = []
-        for w in file:
-            word = re.sub(r'\W+', '', w)
-            stopWords.append(word)
-        if '.txt' in fileString:
-            return self.txtFile(fileString, stopWords)
-        elif '.csv' in fileString:
-            return self.csvFile(fileString, stopWords)
-        else:
-            print('Formato de archivo no soportado')
-
-    def txtFile(self, filename, stopWords):
-        f = open(filename, "r")
-        words = []
-        for x in f:
-            proc = re.sub(r'\W+', ' ', x)
-            notFiltered = proc.lower().split()
-            filtered = [x for x in notFiltered if x not in stopWords]
-            words.append(filtered)
-        return words
-
-    def csvFile(self, filename):
-        f = open(filename)
-        csv_f = csv.reader(f)
-        words = []
-        finalWords = []
-        for row in csv_f:
-            proc = row[0].split(',')
-            for i in proc:
-                word = re.sub(r'\W+', '', i).lower()
-                words.append(word)
-            finalWords.append(words)
+    def process(*args):
+        if len(args) == 4 and isinstance(args[1], str) and isinstance(args[2], str) and isinstance(args[3], list):
+            file = open(args[2], "r")
+            stopWords = []
+            for w in file:
+                word = re.sub(r'\W+', '', w)
+                stopWords.append(word)
+            contador = 1
+            f = open(args[1], "r")
             words = []
-        return finalWords
+            for x in f:
+                print(contador)
+                if not contador in args[3]:
+                    proc = re.sub(r'\W+', ' ', x)
+                    notFiltered = proc.lower().split()
+                    line = ""
+                    for item in notFiltered:
+                        line+= item + " "
+                    print(line)
+                    filtered = [x for x in notFiltered if x not in stopWords]
+                    words.append(filtered)
+                else:
+                    contador+= 1
+            return words
     
-
+        elif len(args) == 3 and isinstance(args[1], str) and isinstance(args[2], str):
+            file = open(args[2], "r")
+            stopWords = []
+            for w in file:
+                word = re.sub(r'\W+', '', w)
+                stopWords.append(word)
+            
+            f = open(args[1], "r")
+            words = []
+            for x in f:
+                proc = re.sub(r'\W+', ' ', x)
+                notFiltered = proc.lower().split()
+                filtered = [x for x in notFiltered if x not in stopWords]
+                words.append(filtered)
+            return words
+        
+            
+        elif len(args) == 2 and isinstance(args[1], str):
+            f = open(args[1], "r")
+            words = []
+            for x in f:
+                proc = re.sub(r'\W+', ' ', x)
+                words.append(proc.lower().split())
+            return words
+    
+    
     
